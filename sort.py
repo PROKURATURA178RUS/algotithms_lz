2a0b194eae0f3e48d188f6e8028fe6c9983228fe
PROKURATURA178RUS <priora05tonirovkas@tutamail.com>
2024-11-30 01:26:09 +0300

    2commit

--- sort.py
+++ sort.py
@@ -1,42 +1,38 @@
-import random # Добавим библиотеку random
+import random as ran
 
-print('Сортировка пузырьком:') # Создадим список для сортировки пузырьком
-bubble = [random.randint(0, 1_000_000) for _ in range(100)]
-bubble_n = bubble.copy()
-bubblesravnenie = 0
-n = len(bubble)
-print('Исходный список:',bubble)
+unsorted_list_1 = [ran.randint(0, 1000000) for t in range(100)] #случайные элементы из указанного диапазона
+print(unsorted_list_1)
+list_2 = unsorted_list_1.copy()
+list_1 = unsorted_list_1.copy()
 
+#сортировка пузырьком
+score1 = 0 #начинаем подсчет с 0 количество сравнений
+stop_flag = True # стоп флаг если он имеет значение true запускает цикл
+while stop_flag:
+    stop_flag = False # меняем значение true, если программа не сделает изменене она завершиться 
+    print(list_1) #промежуточный
+    for i in range(len(list_1) - 1): # -1 потому что в дальнейшем мы сравниваем с i + 1 
+        score1 += 1 #прибавляем каждый цикл +1
+        if list_1[i] < list_1[i + 1]: 
+            ap = list_1[i] 
+            list_1[i] = list_1[i + 1] #меняем местами
+            list_1[i + 1] = ap #меняем местами
+            stop_flag = True
 
-for i in range(n):          # Цикл для сортировки пузырьком
-    for j in range(0, n-i-1):
-        bubblesravnenie += 1
-        if bubble_n[j] < bubble_n[j+1]:  
-            bubble_n[j], bubble_n[j+1] = bubble_n[j+1], bubble_n[j]
-    print(f"Шаг {i + 1}: {bubble_n}")
-
-
-print("Отсортированный список (пузырьком):", bubble_n) # Вывод данных
-print("Количество сравнений (пузырьком):", bubblesravnenie)
-
-
-print('Сортировка выбором:') # Создадим список для сортировки выбором
-choose = [random.randint(0, 1_000_000) for _ in range(100)]
-choose_n = choose.copy()
-choosesravnenie = 0
-n = len(choose)
-print('Исходный список:',choose)
-
-
-for i in range(n):     # Цикл для сортировки выбором
-    max_index = i
-    for j in range(i + 1, n):
-        choosesravnenie += 1
-        if choose_n[j] > choose_n[max_index]:  
-            max_index = j
-    choose_n[i], choose_n[max_index] = choose_n[max_index], choose_n[i]
-    print(f"Шаг {i + 1}: {choose_n}")
-
-
-print("Отсортированный список (выбором):", choose_n) # Вывод данных
-print("Количество сравнений (выбором):", choosesravnenie)
+#сортировка выбором
+score = 0
+for a in range(0 ,len(list_2)):
+    min = a
+    print(list_2) #промежуточный
+    for j in range(a + 1,len(list_2)):
+        score += 1
+        if list_2[j] > list_2[min]:
+            min = j
+    b = list_2[a]
+    list_2[a] = list_2[min]
+    list_2[min] = b
+print(f"НЕ СОРТИРОВАННЫЙ СПИСОК:{unsorted_list_1}")
+print(f"СОРТИРОВАННЫЙ СПИСОК ПУЗЫРЬКОМ:{list_1}")
+print(f"СОРТИРОВАННЫЙ СПИСОК ВЫБОРОМ:{list_2}")
+print(f'количество сравнений в сортировке пузырьком:{score1}')
+print(f'количество сравнений в сортировке выборкой:{score}')
